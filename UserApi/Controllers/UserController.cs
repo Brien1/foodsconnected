27418a77ca92816b9232716152ca84da80d19911
40314811 <bhall06@qub.ac.uk>
2022-11-17 18:07:15 +0000

    better instructions and documentation

--- UserApi/Controllers/UserController.cs
+++ UserApi/Controllers/UserController.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
 using TodoApi.Models;
-
 namespace foods_connected_brien.Controllers
 {
 
@@ -19,9 +19,14 @@ namespace foods_connected_brien.Controllers
         {
             _context = new UserContext();
         }
-        
+
         // GET: api/User
         [HttpGet]
+        [SwaggerOperation(
+            Summary = "Fetches list of users",
+            Description = "Returns a list of users which may be empty if database is empty.\n",
+            Tags = new[] { "Fetch User" }
+        )]
         public async Task<ActionResult<IEnumerable<User>>> GetUser()
         {
             return await _context.User.ToListAsync();
@@ -29,6 +34,11 @@ namespace foods_connected_brien.Controllers
 
         // GET: api/User/5
         [HttpGet("{id}")]
+        [SwaggerOperation(
+            Summary = "Fetches a specific User",
+            Description = "Fetch a User from the database with parameters {userId: long} \n\nReturns User{ userId: long, username: string}.\n\n If the userId does not exist returns NotFound() error ",
+            Tags = new[] { "Fetch User" }
+        )]
         public async Task<ActionResult<User>> GetUser(long id)
         {
             var user = await _context.User.FindAsync(id);
@@ -44,10 +54,15 @@ namespace foods_connected_brien.Controllers
         // PUT: api/User/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
+        [SwaggerOperation(
+            Summary = "Enters a new username onto an existing userID in the database",
+            Description = "Enters a new User into the database with parameters {userId: long, username: string} \n userId is the existing users ID, username is the new username being entered\n\n On success returns CreatedAction containing the details of the entry. \n Username cannot equal another users name else BadRequest is thrown",
+            Tags = new[] { "Update username" }
+        )]
         public async Task<ActionResult<User>> PutUser(long id, String new_username)
         {
             var f = await _context.User.ToListAsync();
-            if (id != id || f.Any((e)=>{return e.username.Equals(new_username) ;}))
+            if (id != id || f.Any((e) => { return e.username.Equals(new_username); }))
             {
                 return BadRequest();
             }
@@ -66,36 +81,45 @@ namespace foods_connected_brien.Controllers
             }
             catch (DbUpdateConcurrencyException)
             {
-            
-               
+
+
             }
 
             return u;
         }
 
-        // POST: api/User
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+
         [HttpPost]
+        [SwaggerOperation(
+            Summary = "Enters a new User into the database",
+            Description = "Enters a new User into the database with parameters {user: User} \n\n On success returns CreatedAction containing the details of the entry. \n\n Username cannot equal another users name else BadRequest is thrown",
+            Tags = new[] { "Insert User" }
+        )]
         public async Task<ActionResult<User>> PostUser(User user)
-        {   
+        {
 
             var f = await _context.User.ToListAsync();
-            if (f.Any((e)=>{return e.username.Equals(user.username) ;}))
+            if (f.Any((e) => { return e.username.Equals(user.username); }))
             {
                 return BadRequest("username exists");
-            } 
+            }
 
             else
             {
-               _context.User.Add(user);
+                _context.User.Add(user);
                 await _context.SaveChangesAsync();
-                return CreatedAtAction("GetUser", new { id = user.userId }, user);  
+                return CreatedAtAction("GetUser", new { id = user.userId }, user);
             }
-           
+
         }
 
         // DELETE: api/User/5
         [HttpDelete("{id}")]
+        [SwaggerOperation(
+            Summary = "Removes a User from the database",
+            Description = "Removes a User from the database with parameters {id: userId} \n\n On success returns the deleted user User{userId: long, username: string}. \n\n If userId does not exist returns NotFound() warning",
+            Tags = new[] { "Delete User" }
+        )]
         public async Task<ActionResult<User>> DeleteUser(long id)
         {
             var user = await _context.User.FindAsync(id);
@@ -107,7 +131,7 @@ namespace foods_connected_brien.Controllers
             _context.User.Remove(user);
             await _context.SaveChangesAsync();
             User f = System.Text.Json.JsonSerializer.Deserialize<User>(r);
-            return f ;
+            return f;
         }
 
         private bool UserExists(long id)
--- UserApi/Program.cs
+++ UserApi/Program.cs
@@ -4,9 +4,10 @@ namespace UserApi.ProgramMain;
 public class Program
 {
 
-    public static WebApplication create(String[]? args) {
-        
-        Console.WriteLine("############ ",args.ToString());
+    public static WebApplication create(String[]? args)
+    {
+
+        Console.WriteLine("############ ", args.ToString());
         // Display the number of command line arguments.
         var builder = WebApplication.CreateBuilder(args);
         var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
@@ -14,29 +15,38 @@ public class Program
         builder.Services.AddControllers();
         builder.Services.AddDbContext<UserContext>();
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(c =>
+        {
+
+
+            c.EnableAnnotations();
+        });
         builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
+        {
+            options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
                           policy.WithOrigins(
                                               "http://localhost:5000").AllowAnyHeader().AllowAnyMethod();
                       });
-});
+
+
+
+
+        });
         var app = builder.Build();
 
         // if (app.Environment.IsDevelopment())
         // {
-            app.UseSwagger(options =>
-            {
-                options.SerializeAsV2 = true;
-            });
-            app.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-                options.RoutePrefix = string.Empty;
-            });
+        app.UseSwagger(options =>
+        {
+            options.SerializeAsV2 = true;
+        });
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = string.Empty;
+        });
         // }
         app.UseHttpsRedirection();
         app.UseRouting();
@@ -49,7 +59,7 @@ public class Program
 
     static void Main(string[] args)
     {
-       
+
 
         WebApplication app = create(args);
         app.Run();
